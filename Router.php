cf90534be9ce7cd59432519338291a5f11953f50
IOU4 <ouchaibimad000@gmail.com>
2022-05-20 11:36:01 +0100

    update router and .env content

--- Router.php
+++ Router.php
@@ -5,35 +5,50 @@ class Router
   private $method;
   private $uri;
 
+
   public function __construct()
   {
     $this->method = $_SERVER['REQUEST_METHOD'];
     $this->uri = $_SERVER['REQUEST_URI'];
   }
 
-  public function get($route, $callable)
+  public function get(string $route, callable $callable)
   {
-    $parsed_url = parse_url($this->uri);
-    $query = [];
-    if (isset($parsed_url['query'])) {
-      parse_str($parsed_url['query'], $query);
-    }
-    if ('/api' . $route == $parsed_url['path'] && $this->method == 'GET') {
-      $callable($query);
+    $path = parse_url($this->uri, PHP_URL_PATH);
+    $query = parse_url($this->uri, PHP_URL_QUERY);
+    if ($query) parse_str($query, $query);
+    if ('/api' . $route == $path && $this->method == 'GET') {
+      call_user_func($callable, $query);
       die();
     }
   }
 
-  public function post($route, $callable)
+  public function post(string $route, callable $callable)
   {
-    $path = parse_url($this->uri)['path'];
+    $path = parse_url($this->uri, PHP_URL_PATH);
     if ('/api' . $route == $path && $this->method == 'POST') {
-      if (empty($_SESSION['user_id']) && $route != '/login' && $route != '/singup') {
-        die(json_encode(['logged' => false]));
-      }
+      $data = empty($_POST) ? json_decode(file_get_contents('php://input'), true) : $_POST;
+      call_user_func($callable, $data);
+      die();
+    }
+  }
 
-      $data = $_POST;
-      $callable($data);
+  public function patch(string $route, callable $callable)
+  {
+    $path = parse_url($this->uri, PHP_URL_PATH);
+    if ('/api' . $route == $path && $this->method == 'PATCH') {
+      $data = json_decode(file_get_contents('php://input'));
+      call_user_func($callable, $data);
+      die();
+    }
+  }
+
+  public function delete(string $route, callable $callable)
+  {
+    $path = parse_url($this->uri, PHP_URL_PATH);
+    if ('/api' . $route == $path && $this->method == 'DELETE') {
+      $data = json_decode(file_get_contents('php://input'));
+      call_user_func($callable, $data);
       die();
     }
   }
